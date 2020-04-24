eef053491601cb426d82437e3be8ff099ffdcafe
Roberto Kai Hsi Hoo <prof.robertohoo@gmail.com>
2020-04-24 13:34:30 -0300

    ok

--- Examples/Example-matrix/Program.cs
+++ Examples/Example-matrix/Program.cs
@@ -6,27 +6,32 @@ using NLinear;
 
 namespace Test
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            //Declare a 4x4 matrix
-            Matrix44<double> m44 = new Matrix44<double>(6, -7, 10, 4, 0, 3, -1, 8, 0, 5, -7, 0, 1, 2, 7, 6);
-            //Declare an identity matrix
-            Matrix44<double> id = Matrix44<double>.Identity(1.0);
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         //Declare a 4x4 matrix
+         Matrix44<double> m44 = new Matrix44<double>(6, -7, 10, 4, 0, 3, -1, 8, 0, 5, -7, 0, 1, 2, 7, 6);
+         //Declare an identity matrix
+         Matrix44<double> id4 = Matrix44<double>.Identity(1.0);
+         Console.WriteLine(m44);
+         Matrix44<double> d = m44.Inverse(1.0);
+         Console.WriteLine("\n" + d*m44); 
+         Console.WriteLine(m44 * id4 == m44); //True
+         Console.WriteLine(id4 * m44 == m44); //True
 
-            Console.WriteLine(m44 * id == m44); //True
-            Console.WriteLine(id * m44 == m44); //True
+         //Declare a 3x3 matrix
+         Matrix33<double> m33 = new Matrix33<double>(6, -7, 10, 0, 3, -1, 0, 5, 2);
 
-            //Declare a 3x3 matrix
-            Matrix33<double> m33 = new Matrix33<double>(6, -7, 10, 0, 3, -1, 0, 5, -7);
+         Matrix33<double> m33Inv = m33.Inverse(1.0);
+         Matrix33<double> c = m33 * m33Inv;
 
-            Matrix33<double> m33Inv = m33.Inverse(1.0);
-            Matrix33<double> id2 = m33 * m33Inv;
+         Console.WriteLine("\n" + c); //True
+         
+         Console.WriteLine(c == Matrix33<double>.Identity(1.0)); //True
+         Console.WriteLine(m44); //True
 
-            Console.WriteLine(id2 == Matrix33<double>.Identity(1.0)); //True
-
-            Console.ReadKey();
-        }
-    }
+         Console.ReadKey();
+      }
+   }
 }
